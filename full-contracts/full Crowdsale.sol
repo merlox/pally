5ce3f26dfadadb0534edd3184ce079ef4a7b5581
merlox <merlodixcontrol@gmail.com>
2017-10-12 11:56:56 +0200

    Updated full crowdsale the refund vault

--- full-contracts/full Crowdsale.sol
+++ full-contracts/full Crowdsale.sol
@@ -242,7 +242,6 @@ contract StandardToken is ERC20, BasicToken {
  *
  * Simple ERC20 Token example, with pausable token creation
  **/
-
 contract PausableToken is StandardToken, Pausable {
 
   function transfer(address _to, uint _value) whenNotPaused returns (bool) {
@@ -251,6 +250,59 @@ contract PausableToken is StandardToken, Pausable {
 
   function transferFrom(address _from, address _to, uint _value) whenNotPaused returns (bool) {
     return super.transferFrom(_from, _to, _value);
+  }
+}
+
+
+/**
+ * @title RefundVault
+ * @dev This contract is used for storing funds while a crowdsale
+ * is in progress. Supports refunding the money if crowdsale fails,
+ * and forwarding it if crowdsale is successful.
+ */
+contract RefundVault is Ownable {
+  using SafeMath for uint256;
+
+  enum State { Active, Refunding, Closed }
+
+  mapping (address => uint256) public deposited;
+  address public wallet;
+  State public state;
+
+  event Closed();
+  event RefundsEnabled();
+  event Refunded(address indexed beneficiary, uint256 weiAmount);
+
+  function RefundVault(address _wallet) {
+    require(_wallet != 0x0);
+    wallet = _wallet;
+    state = State.Active;
+  }
+
+  function deposit(address investor) onlyOwner payable {
+    require(state == State.Active);
+    deposited[investor] = deposited[investor].add(msg.value);
+  }
+
+  function close() onlyOwner {
+    require(state == State.Active);
+    state = State.Closed;
+    Closed();
+    wallet.transfer(this.balance);
+  }
+
+  function enableRefunds() onlyOwner {
+    require(state == State.Active);
+    state = State.Refunding;
+    RefundsEnabled();
+  }
+
+  function refund(address investor) {
+    require(state == State.Refunding);
+    uint256 depositedValue = deposited[investor];
+    deposited[investor] = 0;
+    investor.transfer(depositedValue);
+    Refunded(investor, depositedValue);
   }
 }
 
